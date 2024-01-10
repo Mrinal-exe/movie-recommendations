95bbb52b206db51293190a951b0edf7b36547dba
Mrinal Singh <harshkr918@gmail.com>
2024-01-10 15:26:03 +0530

    Added a function to generate random movies and updated columns.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 from src.data_loader import load_data, get_name_list
 from src.recommendation import get_recommendation
 
@@ -6,26 +7,31 @@ movie_data = load_data()
 movie_name_list = get_name_list()
 
 st.header("Movie Recommendations")
+st.subheader('Search a movie to get recommendations.')
 selected_movie = st.selectbox('Select your favourite movie',options=movie_name_list)
 
+if st.button('Suggest Random'):
+    selected_movie = movie_name_list[random.randint(0, len(movie_name_list))]
+
+recommended_movie_names,recommended_movie_posters = get_recommendation(selected_movie)
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.image(recommended_movie_posters[0], caption=recommended_movie_names[0])
+with col2:
+    st.image(recommended_movie_posters[1], caption=recommended_movie_names[1])
+with col3:
+    st.image(recommended_movie_posters[2], caption=recommended_movie_names[2])
+with col4:
+    st.image(recommended_movie_posters[3], caption=recommended_movie_names[3])
 
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = get_recommendation(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.image(recommended_movie_posters[4], caption=recommended_movie_names[4])
+with col2:
+    st.image(recommended_movie_posters[5], caption=recommended_movie_names[5])
+with col3:
+    st.image(recommended_movie_posters[6], caption=recommended_movie_names[6])
+with col4:
+    st.image(recommended_movie_posters[7], caption=recommended_movie_names[7])
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
--- src/recommendation.py
+++ src/recommendation.py
@@ -19,7 +19,7 @@ def get_recommendation(user_input):
     similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
     recommended_movie_names = []
     recommended_movie_posters = []
-    for i in similar_movies[1:6]:
+    for i in similar_movies[1:9]:
         # fetch the movie poster
         recommended_movie_posters.append(movie_data.iloc[i[0]].Poster_Url)
         recommended_movie_names.append(movie_data.iloc[i[0]].Title)
